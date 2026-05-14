a254131920afc127966f334e7673bc7719889162
andresch0502 <andri.bader@students.fhnw.ch>
2026-05-15 00:50:02 +0200

    Add ECTS parameter to create_subject and delete_subject

--- study_planner/services/subject_service.py
+++ study_planner/services/subject_service.py
@@ -8,9 +8,18 @@ class SubjectService:
         statement = select(Subject)
         return list(session.exec(statement))
 
-    def create_subject(self, session: Session, name: str) -> Subject:
-        subject = Subject(name=name)
+    def create_subject(self, session: Session, name: str, ects: int = 0) -> Subject:
+        subject = Subject(name=name, ects=ects)
         session.add(subject)
         session.commit()
         session.refresh(subject)
         return subject
+
+    def delete_subject(self, session: Session, subject_id: int) -> bool:
+        subject = session.get(Subject, subject_id)
+        if subject is None:
+            return False
+
+        session.delete(subject)
+        session.commit()
+        return True
