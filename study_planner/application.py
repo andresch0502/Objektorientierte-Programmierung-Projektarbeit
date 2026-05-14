e0f2203db2905456ea7f53f02313e8f888774000
andresch0502 <andri.bader@students.fhnw.ch>
2026-05-14 09:50:29 +0200

    Connect application to home page

--- study_planner/application.py
+++ study_planner/application.py
@@ -2,6 +2,7 @@ from nicegui import ui
 
 from study_planner.data_access.db import Database
 import study_planner.domain.models  # ensures SQLModel tables are registered
+from study_planner.ui.pages import show_home_page
 
 
 database = Database()
@@ -9,5 +10,5 @@ database = Database()
 
 def start() -> None:
     database.init_schema()
-    ui.label("StudyPlanner")
+    show_home_page()
     ui.run()
