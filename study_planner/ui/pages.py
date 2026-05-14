868aa1c83d0a52b1a7e360201d9590f85b042dfa
andresch0502 <andri.bader@students.fhnw.ch>
2026-05-14 10:17:42 +0200

    Show subjects on home page

--- study_planner/ui/pages.py
+++ study_planner/ui/pages.py
@@ -1,7 +1,13 @@
 from nicegui import ui
 
-from study_planner.ui.controllers import get_app_title
+from study_planner.ui.controllers import get_app_title, get_subjects
 
 
 def show_home_page() -> None:
     ui.label(get_app_title())
+
+    ui.label("Subjects")
+
+    subjects = get_subjects()
+    for subject in subjects:
+        ui.label(subject.name)
