5b1beaf756ba3d5881d9defcc1936f4bf7c188e0
andresch0502 <andri.bader@students.fhnw.ch>
2026-05-14 12:09:34 +0200

    Rename start function to setup_app

--- study_planner/application.py
+++ study_planner/application.py
@@ -1,5 +1,3 @@
-from nicegui import ui
-
 from study_planner.data_access.db import Database
 import study_planner.domain.models  # ensures SQLModel tables are registered
 from study_planner.ui.pages import show_home_page
@@ -8,7 +6,6 @@ from study_planner.ui.pages import show_home_page
 database = Database()
 
 
-def start() -> None:
+def setup_app() -> None:
     database.init_schema()
     show_home_page()
-    ui.run()
