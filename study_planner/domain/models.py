238fc1b0616a5eb09a2d739273131efd18d93d05
andresch0502 <andri.bader@students.fhnw.ch>
2026-05-14 09:44:05 +0200

    Add Task model

--- study_planner/domain/models.py
+++ study_planner/domain/models.py
@@ -1,3 +1,4 @@
+from datetime import date
 from typing import Optional
 
 from sqlmodel import Field, SQLModel
@@ -7,3 +8,12 @@ class Subject(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
     description: str = ""
+
+
+class Task(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    title: str
+    description: str = ""
+    deadline: Optional[date] = None
+    is_completed: bool = False
+    subject_id: Optional[int] = Field(default=None, foreign_key="subject.id")
