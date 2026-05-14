affe704c2635f3503804645423ed1172cefad726
andresch0502 <andri.bader@students.fhnw.ch>
2026-05-14 10:16:43 +0200

    Add subject loading to UI controller

--- study_planner/ui/controllers.py
+++ study_planner/ui/controllers.py
@@ -1,2 +1,16 @@
+from study_planner.data_access.db import Database
+from study_planner.domain.models import Subject
+from study_planner.services.subject_service import SubjectService
+
+
+database = Database()
+subject_service = SubjectService()
+
+
 def get_app_title() -> str:
     return "StudyPlanner"
+
+
+def get_subjects() -> list[Subject]:
+    with database.session_scope() as session:
+        return subject_service.get_all_subjects(session)
