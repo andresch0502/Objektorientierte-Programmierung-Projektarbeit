b7a52a61fb31ce93592327c9a7242bfc5eae95f2
andresch0502 <andri.bader@students.fhnw.ch>
2026-05-14 11:14:27 +0200

    Add StudySession model

--- study_planner/domain/models.py
+++ study_planner/domain/models.py
@@ -17,3 +17,11 @@ class Task(SQLModel, table=True):
     deadline: Optional[date] = None
     is_completed: bool = False
     subject_id: Optional[int] = Field(default=None, foreign_key="subject.id")
+
+
+class StudySession(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    session_date: date
+    duration_minutes: int
+    notes: str = ""
+    subject_id: Optional[int] = Field(default=None, foreign_key="subject.id")
