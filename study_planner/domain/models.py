e668a0ef472861711c38d4953b2104c71f962440
andresch0502 <andri.bader@students.fhnw.ch>
2026-05-14 12:42:21 +0200

    Add notes field to Task and StudySession models

--- study_planner/domain/models.py
+++ study_planner/domain/models.py
@@ -7,15 +7,14 @@ from sqlmodel import Field, SQLModel
 class Subject(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
-    description: str = ""
 
 
 class Task(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     title: str
-    description: str = ""
     deadline: Optional[date] = None
     is_completed: bool = False
+    notes: str = ""
     subject_id: Optional[int] = Field(default=None, foreign_key="subject.id")
 
 
