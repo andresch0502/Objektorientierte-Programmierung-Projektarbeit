585c8a7e1ba5114cd456aec2bb12c458520ca3e9
andresch0502 <andri.bader@students.fhnw.ch>
2026-05-14 10:21:34 +0200

    Add task service

---
+++ study_planner/services/task_service.py
@@ -0,0 +1,26 @@
+from sqlmodel import Session, select
+
+from study_planner.domain.models import Task
+
+
+class TaskService:
+    def get_all_tasks(self, session: Session) -> list[Task]:
+        statement = select(Task)
+        return list(session.exec(statement))
+
+    def create_task(
+        self,
+        session: Session,
+        title: str,
+        description: str = "",
+        subject_id: int | None = None,
+    ) -> Task:
+        task = Task(
+            title=title,
+            description=description,
+            subject_id=subject_id,
+        )
+        session.add(task)
+        session.commit()
+        session.refresh(task)
+        return task
